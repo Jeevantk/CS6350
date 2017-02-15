d085575bd352c95ccf43dd6050655777dbfd1da0
Jeevantk <jeevan.thomaskoshy@gmail.com>
2017-02-15 23:28:00 +0530

    Added two papers

--- H_tranform.cpp
+++ H_tranform.cpp
@@ -1,8 +1,16 @@
 #include "opencv/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/xfeatures2d.hpp"
+
 #include "iostream"
 
 using namespace std;
 using namespace cv;
+using namespace xfeatures2d;
 
-class 
+
+int main()
+{
+	Ptr<Feature2D> sift=SIFT::create();
+	vector<KeyPoint> keypoints_1,keypoints_2;
+}
